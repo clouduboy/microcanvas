48d234024cdb1df1e412895ab6839befacee7190
Flaki <git@flaki.hu>
2017-07-21 12:35:50 +0200

    Beta: Sound support re-added

--- microcanvas.js
+++ microcanvas.js
@@ -688,3 +688,339 @@ const static unsigned char font[] PROGMEM =
 
   window.MicroCanvas = MicroCanvas;
 })();
+
+
+
+
+'use strict';
+
+(function () {
+
+  // Table of midi note frequencies * 2
+  //   They are times 2 for greater accuracy, yet still fits in a word.
+  //   Generated from Excel by =ROUND(2*440/32*(2^((x-9)/12)),0) for 0<x<128
+  // The lowest notes might not work, depending on the Arduino clock frequency
+  // Ref: http://www.phy.mtu.edu/~suits/notefreqs.html
+  // From: Arduboy's audio.cpp
+  // https://github.com/Arduboy/Arduboy/blob/master/audio.cpp @ bac3ae9 on Jun 2 2015
+  var NOTES = [
+    16,17,18,19,21,22,23,24,26,28,29,31,33,35,37,39,41,44,46,49,52,55,58,62,65,
+    69,73,78,82,87,92,98,104,110,117,123,131,139,147,156,165,175,185,196,208,220,
+    233,247,262,277,294,311,330,349,370,392,415,440,466,494,523,554,587,622,659,
+    698,740,784,831,880,932,988,1047,1109,1175,1245,1319,1397,1480,1568,1661,1760,
+    1865,1976,2093,2217,2349,2489,2637,2794,2960,3136,3322,3520,3729,3951,4186,
+    4435,4699,4978,5274,5588,5920,6272,6645,7040,7459,7902,8372,8870,9397,9956,
+    10548,11175,11840,12544,13290,14080,14917,15804,16744,17740,18795,19912,21096,
+    22351,23680,25088
+  ];
+
+  function ClouduboySynth() {
+    // create web audio api context
+    var ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+    // create main gain node
+    var mainGain = ctx.createGain();
+
+    mainGain.connect(ctx.destination);
+    mainGain.gain.value = .33;
+
+    // Access to audioContext (TODO: is this needed?)
+    Object.defineProperty(this, 'audioCtx', {
+      enumerable: true,
+      get: function() { return ctx; }
+    });
+
+    // Current time in audioContext
+    Object.defineProperty(this, 'playHead', {
+      enumerable: true,
+      get: function() { return ctx.currentTime; }
+    });
+
+    // Create a new Oscillator and pipe it into the main gain
+    this.osc = function(freq, type) {
+      var osc = ctx.createOscillator();
+
+      osc.type = type || 'square';
+      osc.frequency.value = freq;
+
+      osc.connect(mainGain);
+      return osc;
+    }
+  }
+
+  ClouduboySynth.prototype = Object.create(null);
+
+  ClouduboySynth.prototype.pause = function() {
+    this.audioCtx.pause();
+  }
+  ClouduboySynth.prototype.play = function() {
+    this.audioCtx.resume();
+  }
+  ClouduboySynth.prototype.stop = function() {
+    this.audioCtx.close();
+  }
+
+  ClouduboySynth.prototype.playNote = function(note, at, length) {
+    var osc,
+        freq = NOTES[ note>127 ? 127 : note ]/2;
+
+    // Create oscillator w/ freq frequency (default type is 'square')
+    osc = this.osc(freq);
+
+    // Start note at the 'at' playhead-position
+    if (typeof at !== 'undefined') {
+      // Start now
+      if (at === 0) at = this.playHead;
+
+      osc.start(at);
+
+      // Stop note at after 'length' ms
+      if (typeof length !== 'undefined') {
+        osc.stop(at+length);
+      }
+
+    // start right now
+    } else {
+      osc.start();
+    }
+
+    return osc;
+  }
+
+  ClouduboySynth.prototype.stopNote = function(osc, at) {
+    // Stop note at the 'at' playhead-position
+    if (typeof at !== 'undefined') {
+      osc.stop(at);
+
+    // stop playing right now
+    } else {
+      osc.stop();
+    }
+  }
+
+  ClouduboySynth.freq = function(note) {
+    return NOTES[ note>127 ? 127 : note ]/2;
+  }
+
+  self.ClouduboySynth = ClouduboySynth;
+})();
+(function () {
+  'use strict';
+
+  // Synth control command constants (from audio.h)
+  var TUNE_OP_PLAYNOTE = 0x90;  /* play a note: low nibble is generator #, note is next byte */
+  var TUNE_OP_STOPNOTE = 0x80;  /* stop a note: low nibble is generator # */
+  var TUNE_OP_RESTART  = 0xe0;  /* restart the score from the beginning */
+  var TUNE_OP_STOP     = 0xf0;  /* stop playing */
+
+  // Ported from: Arduboy's audio.cpp & audio.h
+  // https://github.com/Arduboy/Arduboy/blob/master/audio.h @ 7ddb720 on Dec 3 2015
+  // https://github.com/Arduboy/Arduboy/blob/master/audio.cpp @ bac3ae9 on Jun 2 2015
+  function ArduboyScore(score) {
+    // Tab is a parsed score, represents the more flexible in-memory
+    // representation of the binary score/score data
+    var tab;
+
+    // Score speed ratio, allows one to stretch/compress the score
+    // TODO: currently unimplemented/not exposed
+    var speed = 1;
+
+    // Create tab
+    tab = parse(score);
+
+    // Access to "tab" via this.tab
+    Object.defineProperty(this, 'tab', {
+      enumerable: true,
+      get: function() { return tab; }
+    });
+
+
+    // Parses string/binary score data into internal "tab" representation
+    function parse(score) {
+      // Binary score data
+      var pgm_read_byte;
+
+      // Already in binary format
+      if (score instanceof Array) {
+        pgm_read_byte = score;
+
+      // Parse score into binary format
+      } else {
+        //pgm_read_byte = util.cleanComments(score).split(/,/).map(function(n) {
+        pgm_read_byte = score.replace(/(\/\/[^\n]*\n|\/\*(.*?\*\/))/g, '').split(/,/).map(function(n) {
+          return parseInt(n);
+        });
+      }
+
+      // TODO: create index for source data
+
+      // Current score position
+      var score_cursor, score_start, tune_playing;
+
+      // Current play context time
+      var playhead;
+
+      // Concurrent channels (3)
+      var chn = [];
+
+      // Init & play
+      var tab = [],
+          tabch = [];
+
+      score_cursor = score_start = playhead = 0;
+      tune_playing = true;
+
+      // Play whole song
+      while (score_cursor < pgm_read_byte.length) {
+        step();
+      }
+
+      // Done
+      return tab;
+
+
+
+      /* Functions used for parsing */
+      function __play_note(channel, note) {
+        tab.push(
+          { op: "play-note", note: note, freq: ClouduboySynth.freq(note), ch: channel, score: [score_cursor-2, score_cursor-1], time: playhead }
+        );
+
+        // latest note in channel
+        tabch[ channel ] = tab[ tab.length-1 ];
+      }
+
+      function __stop_note(channel) {
+        tab.push(
+          { op: "stop-note", stops: tabch[channel], ch: channel, score: [score_cursor-1, score_cursor-1], time: playhead }
+        );
+
+        // link stop-note to the start-note
+        if (tabch[channel]) {
+          tabch[channel].stop = tab[tab.length-1];
+          tabch[channel] = null;
+        }
+      }
+
+      function __wait(ms) {
+        ms *= speed; // speed up/slow down TODO: expose this
+
+        tab.push(
+          { op: "wait", duration: ms, score: [score_cursor-2, score_cursor-1], time: playhead }
+        );
+
+        // Advance playhead
+        playhead += ms;
+      }
+
+      // Ported from: audio.cpp
+      function step() {
+        var command, opcode, chan;
+        var duration;
+
+        while (score_cursor < pgm_read_byte.length) {
+          command = pgm_read_byte[score_cursor++];
+          opcode = command & 0xf0;
+          chan = command & 0x0f;
+          if (opcode == TUNE_OP_STOPNOTE) { /* stop note */
+            __stop_note(chan);
+          }
+          else if (opcode == TUNE_OP_PLAYNOTE) { /* play note */
+            __play_note(chan, pgm_read_byte[score_cursor++]);
+          }
+          else if (opcode == TUNE_OP_RESTART) { /* restart score */
+            score_cursor = score_start;
+          }
+          else if (opcode == TUNE_OP_STOP) { /* stop score */
+            tune_playing = false;
+            break;
+          }
+          else if (opcode < 0x80) { /* wait count in msec. */
+            duration = (command << 8) | (pgm_read_byte[score_cursor++]);
+            __wait(duration);
+            //wait_toggle_count = ((unsigned long) wait_timer_frequency2 * duration + 500) / 1000;
+            //if (wait_toggle_count == 0) wait_toggle_count = 1;
+            break;
+          }
+        }
+      } // step()
+
+    } // parse()
+  } // ArduboyScore constructor
+
+
+  ArduboyScore.prototype = Object.create(null);
+
+  ArduboyScore.prototype.play = function() {
+    var ch = [];
+
+    // Create new synth, close old one if one exists
+    if (this.synth) this.synth.stop();
+    var synth = this.synth = new ClouduboySynth();
+
+    // Play score from tab data
+    this.tab.forEach(function(t) {
+      // Play note
+      if (t.op === 'play-note') {
+        ch[t.ch] = synth.playNote(t.note, t.time/1000);
+        // TODO: explore ommiting this, by scheduling the note in the 'stop-note'
+        // handler, via the .starts link right before scheduling the stop
+        // (catch: what if a note is never stopped?)
+
+      // Stop note
+      } else if(t.op === 'stop-note') {
+        if (ch[t.ch]) synth.stopNote(ch[t.ch], t.time/1000);
+        ch[t.ch] = null;
+
+      } // ignore all other tab nodes
+    });
+
+    return this;
+  }
+
+  ArduboyScore.prototype.pause = function() {
+    this.synth.pause();
+  }
+  ArduboyScore.prototype.resume = function() {
+    this.synth.resume();
+  }
+  ArduboyScore.prototype.stop = function() {
+    this.synth.stop();
+  }
+
+  // Global non-instance methods
+  ArduboyScore.make = function(inputs) {
+    var score = [];
+    var PLAY = "0x"+TUNE_OP_PLAYNOTE.toString(16);
+    var STOP = "0x"+TUNE_OP_STOPNOTE.toString(16);
+    var END = "0x"+TUNE_OP_STOP.toString(16);
+
+    var basetime, time, d;
+
+    if (!inputs.length) return "";
+    basetime = inputs[0].t;
+    time = basetime;
+
+    for (var i of inputs) {
+      if (i.t > time) {
+        d = Math.round(i.t - time);
+        score.push(d >> 8, d & 255);
+      }
+      time = i.t;
+
+      if (i.state) {
+        score.push(PLAY);
+        score.push(i.note);
+      } else {
+        score.push(STOP);
+      }
+    }
+
+    score.push(END);
+    console.log( score.join(', ') );
+    return JSON.stringify(inputs);
+  }
+
+  // Expose
+  self.ArduboyScore = ArduboyScore;
+})();
