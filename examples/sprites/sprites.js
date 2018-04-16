87ad335ce9bca85cc0dad7d3dc22bda60d9f31cf
Flaki <git@flaki.hu>
2018-04-16 21:05:57 +0200

    Updated Sprites demo

--- examples/sprites/sprites.js
+++ examples/sprites/sprites.js
@@ -1,75 +1,119 @@
-"use strict";
+'use strict';
 
 let game = new MicroCanvas();
 
-let gfxBats;
 
-let x, y;
-let sx = 1, sy = 1;
 
-let animationSpeed;
+// Assets (graphics sprites, audio etc.) needs to be preloaded
+
+// The bat sprite. It contains two frames with different poses
+const gfxBats = game.loadSprite(`
+  ! bats 16x6x2
+  ##....#..#....##
+  .##...####...##.
+  .###.#.##.#.###.
+  .##############.
+  ...##########...
+  ....##.##.##....
+  ...#..#..#..#...
+  .#.##.####.##.#.
+  ######.##.######
+  ###.########.###
+  #.....####.....#
+  .......##.......
+`);
+
+
+
+// Constants (things that don't change)
+// They get their initial value right here where they are declared
+// (defined), and this value won't ever change during the course of
+// running the rest of the code.
+
+// Animation speed of bat wing flaps (in frames)
+const animationDelay = 8;
+
+
+//
+// VARIABLES (global)
+//
+// The value of these variables can be accessed from anywhere throughout
+// the code and will (at least, it can) change.
+// (Re-)Initialization happens in the setup function, this ensures
+// that when setup() is called again (this is called a "soft" reset)
+// these values are re-set to their initial value (e.g. score is
+// reset to zero, etc.)
+
+// Bat's current x, y coordinates; speed in x & y axes
+let x, y, speedX, speedY;
+
+// The pose (frame) of the bat currently being displayed
+// The value is flipped every "animationDelay" frames
 let cSprite;
 
 
-// Load bat sprite & initialize animation
-game.setup(function(game) {
-  gfxBats = game.loadSprite(`
-    ! bats 16x6x2
-    ##....#..#....##
-    .##...####...##.
-    .###.#.##.#.###.
-    .##############.
-    ...##########...
-    ....##.##.##....
-    ...#..#..#..#...
-    .#.##.####.##.#.
-    ######.##.######
-    ###.########.###
-    #.....####.....#
-    .......##.......
-  `);
+
+//
+// SETUP FUNCTION (runs once every time the program is loaded)
+//
+game.setup(() => {
 
   // Start in the left-side center of the screen
   x = 0;
   y = (game.height-gfxBats.height)/2;
 
-  // Animation speed of bat wing flaps (in frames)
-  animationSpeed = 8;
 
   // Helper variable for flapping animation
   cSprite = 0;
+
+  // Reset speed
+  speedX = 1
+  speedY = 1
 });
 
 
+
+//
+// LOOP FUNCTION (executed 60 times a second while the game is running)
+//
 game.loop(function() {
+  //
+  // UPDATE (update the game state)
+  //
+
   // Update flapping animation
-  if (game.everyXFrames(animationSpeed)) {
-    cSprite = !cSprite;
+  if (game.everyXFrames(animationDelay)) {
+    cSprite = 1 - cSprite;
   }
 
   // Update position
-  x += sx;
-  y += sy;
+  x += speedX;
+  y += speedY;
 
-  // Constrain bat inside screen bounds
-  if (x >= (game.width-gfxBats.width-1) || x<1) sx = -sx;
-  if (y >= (game.height-gfxBats.height-1) || y<1) sy = -sy;
+  // Constrain bat inside screen bounds (bat will "bounce"
+  // whenever it reaches the edges of the screen)
+  if (x >= (game.width-gfxBats.width-1) || x<1) speedX = -speedX;
+  if (y >= (game.height-gfxBats.height-1) || y<1) speedY = -speedY;
 
 
-  // Clear display, redraw background text
+  //
+  // DRAW (display game state)
+  //
+
+  // Clear display
   game.clear();
 
   // Draw some background text
   game.drawText("Sprite\nDemo", 0,0, 3);
 
-  // Draw shadow (unset pixels on screen based on the bitmap)
+  // Draw bat shadow
+  // (draw the bat, slightly offset, with the background color
+  // this helps in distinguishing the bat we'll be drawing on top
+  // from the background)
+  game.eraseImage(gfxBats[cSprite], 2 +x,2 +y);
+  game.eraseImage(gfxBats[cSprite], 0 +x,2 +y);
   // TODO: platform support outside of Arduboy (JS canvas, Tiny Arcade)
-  game.eraseImage(gfxBats[cSprite|0], 0 +x,2 +y);
-  game.eraseImage(gfxBats[cSprite|0], 2 +x,2 +y);
 
-  // Draw Bat
-  game.drawImage(gfxBats[cSprite|0], 1 +x,1 +y);
+  // Draw bat in current pose (use correct frame)
+  game.drawImage(gfxBats[cSprite], 1 +x,1 +y);
 });
-
-
-console.log("MicroCanvas initialized");
--- examples/sprites/sprites.tiny_arcade.cpp
+++ examples/sprites/sprites.tiny_arcade.cpp
@@ -4,6 +4,7 @@
 
 #include <TinyArcade.h>
 
+
 ////// TARGET-SPECIFIC SETUP: TINY_ARCADE //////
 #define screenWidth 96
 #define screenHeight 64
@@ -15,14 +16,17 @@
 #define BLACK 1
 #define WHITE 0
 
+#define LEFT_BUTTON 1
+#define RIGHT_BUTTON 2
+#define A_BUTTON 3
+
 // Currently used color bitdepth (1 - 8 bit, 2 - 16 bit)
 #define BIT_DEPTH 2
 
 // 16bit screenbuffer
 uint8_t buffer[WIDTH * HEIGHT * BIT_DEPTH];
 
-TinyScreen display = TinyScreen(TinyScreenPlus);
-
+TinyScreen _display = TinyScreen(TinyScreenPlus);
 
 
 
@@ -35,47 +39,49 @@ char _microcanvas_textbuffer[32];
 // global state machine
 unsigned int _microcanvas_state;
 
+// global current drawing color
+unsigned int _microcanvas_fill_color = WHITE;
 
-// 16-bit color sprite
-#define GFX_F10_MASKCOLOR 0xde5f
+#define LENGTHOF(x)  (sizeof(x) / sizeof(x[0]))
 
-#define GFX_BATS_WIDTH 16
-#define GFX_BATS_HEIGHT 6
-#define GFX_BATS_FRAMES 2
-#define GFX_BATS_FRAMESIZE  GFX_BATS_WIDTH*GFX_BATS_HEIGHT/GFX_BATS_FRAMES // pretty straightforward here
-
-const uint16_t GFX_BATS[GFX_BATS_WIDTH * GFX_BATS_HEIGHT * GFX_BATS_FRAMES] = {
-  /*16x6x2@16b*/ 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000
-};
-const uint16_t GFX_BATS_MASKCOLOR = 0x0000;
+const uint16_t gfx_bats[] = {
+  /*16x6x2@16b*/ 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000 };
 
 
-////// CUST
-int x = 0;
-int y = 24;
-int sx = 1;
-int sy = 1;
-int animation_speed = 8;
-int c_sprite = 0;
+const byte GFX_BATS_WIDTH = 16;
+const byte GFX_BATS_HEIGHT = 6;
+const byte GFX_BATS_FRAMES = 2;
+const byte GFX_BATS_FRAMESIZE = 96;
+const int GFX_BATS_MASKCOLOR = 0x0000;
+const int ANIMATION_DELAY = 8;
 
+int x;
+int y;
+int speed_x;
+int speed_y;
+int c_sprite;
 
 
 void setup() {
+  _microcanvas_frame_counter = 0;
+
+  // TODO: reset compatibility
+  // cpuLoad() will only be 0 right after boot
   arcadeInit();
   Wire.begin();
 
-  display.begin();
-  display.setFlip(0);
-  display.setBrightness(8);
-  display.setBitDepth(1);
-  //display.setBitDepth(0);
-
-  tiny_arcade_init();
+  _display.begin();
+  _display.setFlip(0);
+  _display.setBrightness(8);
+  _display.setBitDepth(1);
 
 ////// CUSTOM SETUP //////
-
+x = 0;
+y = (HEIGHT - GFX_BATS_HEIGHT) / 2;
+c_sprite = 0;
+speed_x = 1;
+speed_y = 1;
 }
-
 
 void loop() {
   if (!tiny_arcade_nextFrame()) return;
@@ -83,28 +89,29 @@ void loop() {
   ++_microcanvas_frame_counter;
   if (_microcanvas_frame_counter==60000) _microcanvas_frame_counter = 0;
 
+  ////// LOOP CONTENTS TO FOLLOW //////
+
 ////// LOOP CONTENTS TO FOLLOW //////
-if (tiny_arcade_everyXFrames( animation_speed )) {
-  c_sprite = !c_sprite;
+if (tiny_arcade_everyXFrames( ANIMATION_DELAY )) {
+  c_sprite = 1 - c_sprite;
 }
-x += sx;
-y += sy;
-if (x >=  WIDTH-GFX_BATS_WIDTH -1  || x < 1) sx = -sx;
-if (y >= HEIGHT-GFX_BATS_HEIGHT-1 || y < 1) sy = -sy;
+x += speed_x;
+y += speed_y;
+if (x >= ((WIDTH - GFX_BATS_WIDTH) - 1) || x < 1) speed_x = -speed_x;
+if (y >= ((HEIGHT - GFX_BATS_HEIGHT) - 1) || y < 1) speed_y = -speed_y;
 tiny_arcade_clear();
 {
-//arduboy.setTextSize( 3 );
-//arduboy.setCursor( 0, 0 );
-//arduboy.print( "Sprite\nDemo" );
+tiny_arcade_setTextSize( 3 );
+tiny_arcade_setCursor( 0, 0 );
+tiny_arcade_print( "Sprite\nDemo" );
 };
-tiny_arcade_drawBitmap( (0 + x), (2 + y), GFX_BATS + BIT_DEPTH*GFX_BATS_FRAMESIZE*(c_sprite | 0), GFX_BATS_WIDTH, GFX_BATS_HEIGHT, BLACK );
-tiny_arcade_drawBitmap( (2 + x), (2 + y), GFX_BATS + BIT_DEPTH*GFX_BATS_FRAMESIZE*(c_sprite | 0), GFX_BATS_WIDTH, GFX_BATS_HEIGHT, BLACK );
-tiny_arcade_drawBitmap( (1 + x), (1 + y), GFX_BATS + BIT_DEPTH*GFX_BATS_FRAMESIZE*(c_sprite | 0), GFX_BATS_WIDTH, GFX_BATS_HEIGHT, WHITE );
+tiny_arcade_drawBitmap( 2 + x, 2 + y, gfx_bats + GFX_BATS_FRAMESIZE*(c_sprite), GFX_BATS_WIDTH, GFX_BATS_HEIGHT, BLACK );
+tiny_arcade_drawBitmap( 0 + x, 2 + y, gfx_bats + GFX_BATS_FRAMESIZE*(c_sprite), GFX_BATS_WIDTH, GFX_BATS_HEIGHT, BLACK );
+tiny_arcade_drawBitmap( 1 + x, 1 + y, gfx_bats + GFX_BATS_FRAMESIZE*(c_sprite), GFX_BATS_WIDTH, GFX_BATS_HEIGHT, WHITE );
 ////// END OF LOOP CONTENTS //////
 
   tiny_arcade_display();
 }
-
 
 
 
@@ -119,7 +126,7 @@ uint8_t lastFrameDurationMs;
 
 /* Graphics */
 void tiny_arcade_clear() {
-  tiny_arcade_fillScreen(0x00ff);
+  tiny_arcade_fillScreen(0x0000);
 }
 
 void tiny_arcade_fillScreen(uint16_t color) {
@@ -131,10 +138,10 @@ void tiny_arcade_fillScreen(uint16_t color) {
 
 void tiny_arcade_display() {
   // write double-buffer contents
-  display.goTo(0, 0);
-  display.startData();
-  display.writeBuffer(buffer, WIDTH * HEIGHT * 2);
-  display.endTransfer();
+  _display.goTo(0, 0);
+  _display.startData();
+  _display.writeBuffer(buffer, WIDTH * HEIGHT * 2);
+  _display.endTransfer();
 }
 
 //void tiny_arcade_drawBitmap(int16_t x, int16_t y, const uint8_t *gfx, uint8_t w, uint8_t h, uint8_t blending) {
@@ -146,7 +153,7 @@ void tiny_arcade_drawBitmap(int16_t dx, int16_t dy, const uint16_t *gfx, uint8_t
       // Color mode 0 (default)
       // Paint only opaque pixels, skip transparent pixels
       // TODO: transparency handling
-      if (blending == WHITE && color != GFX_BATS_MASKCOLOR) {
+      if (blending == WHITE) {
         buffer[(x + y*WIDTH) * BIT_DEPTH] = color >> 8;
         if (BIT_DEPTH == 2) buffer[(x + y*WIDTH) * BIT_DEPTH + 1] = color;
       }
@@ -213,3 +220,24 @@ bool tiny_arcade_nextFrame()
   post_render = true;
   return post_render;
 }
+
+bool tiny_arcade_pressed(int btn) {
+  switch (btn) {
+    case A_BUTTON:
+      return checkButton(TAButton1);
+    case LEFT_BUTTON:
+      return checkJoystick(TAJoystickLeft);
+    case RIGHT_BUTTON:
+      return checkJoystick(TAJoystickRight);
+  }
+  return false;
+}
+
+
+
+/// DUMMY / UNIMPLEMENTED
+void tiny_arcade_fillRect(int x, int y, int w, int h) {}
+
+void tiny_arcade_setTextSize(int size) {}
+void tiny_arcade_setCursor( int x, int y ) {}
+void tiny_arcade_print( const char* string) {}
